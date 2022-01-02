8cf716f604655e6976dad4ef33114a0bbb2888df
solovyova-1996 <78434580+solovyova-1996@users.noreply.github.com>
2022-01-02 21:07:31 +0600

    work from urls(add mainapp.urls)

--- shoponline/mainapp/views.py
+++ shoponline/mainapp/views.py
@@ -4,4 +4,8 @@ from django.shortcuts import render
 def index(request):
     return render(request,'mainapp/index.html')
 def products(request):
-    return render(request,'mainapp/products.html')
+    context = {
+        'title': 'Каталог',
+        'products': [{'name':''},{'name':''},{'name':''},{'name':''},{'name':''},{'name':''}],
+    }
+    return render(request,'mainapp/products.html',context=context)
