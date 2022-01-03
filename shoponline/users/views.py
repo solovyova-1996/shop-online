db308b9efd6ad82be60f3d20f0233d043d657233
solovyova-1996 <78434580+solovyova-1996@users.noreply.github.com>
2022-01-03 20:20:29 +0600

    add forms.py

--- shoponline/users/views.py
+++ shoponline/users/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 
+from users.forms import UserLoginForm
+
 
 def login(request):
+    form = UserLoginForm()
     context = {
         'title': 'Войти',
+        'form': form,
     }
     return render(request, 'users/login.html', context=context)
 
