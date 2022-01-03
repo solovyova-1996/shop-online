3bf5110ba2b9d677c33bf305b74aefd24c241517
solovyova-1996 <78434580+solovyova-1996@users.noreply.github.com>
2022-01-03 20:58:35 +0600

    update form login

--- shoponline/users/forms.py
+++ shoponline/users/forms.py
@@ -6,4 +6,12 @@ from users.models import User
 class UserLoginForm(AuthenticationForm):
     class Meta:
         model = User
-        fields = ('login', 'password')
+        fields = ('username', 'password')
+
+    def __init__(self, *args, **kwargs):
+        super(UserLoginForm, self).__init__(*args, **kwargs)
+        # добавляем стили для полей формы
+        self.fields['username'].widget.attrs['placeholder'] = 'Введите имя пользователя'
+        self.fields['password'].widget.attrs['placeholder'] = 'Введите пароль'
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control py-4'
--- shoponline/users/views.py
+++ shoponline/users/views.py
@@ -1,10 +1,27 @@
-from django.shortcuts import render
+from django.contrib import auth
+from django.shortcuts import render,HttpResponseRedirect
+from django.urls import reverse
 
 from users.forms import UserLoginForm
 
 
 def login(request):
-    form = UserLoginForm()
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            # получаем введеные пользователем данные
+            username = request.POST['username']
+            password = request.POST['password']
+            # проверяем есть ли в базе данный пользователь
+            user = auth.authenticate(username=username, password=password)
+            # если пользователь есть в базе и активен авторизуем его и перенаправляем на главную страницу
+            if user.is_active:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+        else:
+            print(form.errors)
+    else:
+        form = UserLoginForm()
     context = {
         'title': 'Войти',
         'form': form,
