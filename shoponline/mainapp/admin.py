264732f589ec0fb8f61e2a3849a118c1c04e2ad6
solovyova-1996 <78434580+solovyova-1996@users.noreply.github.com>
2022-01-08 02:02:45 +0600

    work from admin django

--- shoponline/mainapp/admin.py
+++ shoponline/mainapp/admin.py
@@ -3,4 +3,16 @@ from django.contrib import admin
 from mainapp.models import Category, Product
 
 admin.site.register(Category)
-admin.site.register(Product)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    # добавление нужных полей в админке
+    list_display = ('name', 'price', 'quantity', 'category')
+    # отображение полей при просмотре одного продукта
+    fields = ('name','img','description', 'price', 'quantity', 'category',)
+    # поля только для чтения
+    readonly_fields = ('description',)
+# поля, по которомы сортировать
+    ordering = ('name','price')
+    search_fields = ('name',)
