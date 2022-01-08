324e50399f63dfcd7ee37ebe4a231c6e00e967a3
solovyova-1996 <78434580+solovyova-1996@users.noreply.github.com>
2022-01-08 20:58:14 +0600

    add filter from category

--- shoponline/mainapp/urls.py
+++ shoponline/mainapp/urls.py
@@ -4,4 +4,6 @@ from .views import products
 app_name = 'products'
 urlpatterns = [
     path('', products, name='index'),
+    # для фильтрации товаров по категориям
+    path('category/<int:category_id>/',products,name='category')
 ]
--- shoponline/mainapp/views.py
+++ shoponline/mainapp/views.py
@@ -4,11 +4,12 @@ from mainapp.models import Product, Category
 
 
 def index(request):
-    return render(request,'mainapp/index.html')
-def products(request):
-    context = {
-        'title': 'Каталог',
-        'categories': Category.objects.all(),
-        'products': Product.objects.all(),
-    }
-    return render(request,'mainapp/products.html',context=context)
+    return render(request, 'mainapp/index.html')
+
+
+def products(request, category_id=None):
+    # фильтрация продуктов по категориям
+    products = Product.objects.filter(category_id=category_id) if category_id != None else Product.objects.all()
+    context = {'title': 'Каталог', 'categories': Category.objects.all(),
+        'products': products, }
+    return render(request, 'mainapp/products.html', context=context)
